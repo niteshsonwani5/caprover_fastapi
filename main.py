28726b0ef59a9b7cc00b6b6f536c0fc156bb683d
Nitesh Sonwani <niteshsonwani5@gmail.com>
2022-09-27 13:54:01 +0530

    third commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World 123"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
