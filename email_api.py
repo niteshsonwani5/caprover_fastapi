732024aa4205b815d2745bd4cb086c63369b8cd6
Nitesh Sonwani <niteshsonwani5@gmail.com>
2022-10-07 12:56:23 +0530

    changes in api

--- email_api.py
+++ email_api.py
@@ -31,24 +31,13 @@ async def email(request: Request,order_number:str,user_email : str):
         def save(invoice_base64, filename="invoice"):
             with open(filename + ".pdf", 'wb') as file:
                 file.write(base64.b64decode(invoice_base64))
-            with open(filename + ".png", 'wb') as file:
-                file.write(base64.b64decode(invoice_base64))
-
-            with open("imageT.png", "wb") as fh:
-                fh.write(base64.urlsafe_b64decode(invoice_base64))
-
-            # from PIL import Image
-            # from base64 import decodestring
-            #
-            # image = Image.fromstring('RGB', (width, height), decodestring(imagestr))
-            # image.save("foo.png")
 
     result = EasyInvoice.create(data)
     EasyInvoice.save(result["pdf"], order_number)
     sender_email = "order.5guyswholesale@gmail.com"
     receiver_email = "5guyswholesale@gmail.com"
     password = "kdmrszoxmvrpzjcg"
-    subject = f"Order {order_number} confirmed"
+    subject = f"Order #{order_number} confirmed"
     body = f"This is an confirm order of {user_email}"
 
     msg = EmailMessage()
@@ -60,9 +49,9 @@ async def email(request: Request,order_number:str,user_email : str):
     # set the plain text body
     msg.set_content(body)
 
-    with open('invoice.pdf', 'rb') as content_file:
+    with open(f'{order_number}.pdf', 'rb') as content_file:
         content = content_file.read()
-        msg.add_attachment(content, maintype='application', subtype='pdf', filename='invoice123.pdf')
+        msg.add_attachment(content, maintype='application', subtype='pdf', filename=f'{order_number}.pdf')
 
     text = msg.as_string()
 
