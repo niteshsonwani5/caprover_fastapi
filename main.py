9d6b0dd5df11b4b550975e5dd9f26d0f1ceda259
Nitesh Sonwani <niteshsonwani5@gmail.com>
2022-12-13 19:08:40 +0530

    test

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World !"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
